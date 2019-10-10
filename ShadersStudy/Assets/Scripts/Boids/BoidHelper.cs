a4d000aa0f0ef778596d28907a7343e9fd6c4783
Rosie_Mable <sciarrillo.nella@gmail.com>
2019-10-10 20:24:35 +0100

    Boids Game?

---
+++ ShadersStudy/Assets/Scripts/Boids/BoidHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BoidHelper 
+{
+    const int nViewDirections = 300;
+    public static readonly Vector3[] directions;
+
+    static BoidHelper()
+    {
+        directions = new Vector3[BoidHelper.nViewDirections];
+
+        float goldenRatio = (1 + Mathf.Sqrt(5)) / 2; //Formula of golden ratio ( a/b = (a + b)/a )
+        float angleIncrement = Mathf.PI * 2 * goldenRatio;
+
+        for (int i = 0; i < nViewDirections; i++)
+        {
+            float t = (float)i / nViewDirections;
+            float inclination = Mathf.Acos(1 - 2 * t);
+            float azimuth = angleIncrement * i;
+
+            float x = Mathf.Sin(inclination) * Mathf.Cos(azimuth);
+            float y = Mathf.Sin(inclination) * Mathf.Sin(azimuth);
+            float z = Mathf.Cos(inclination);
+            directions[i] = new Vector3(x, y, z);
+        }
+    }
+}
---
+++ ShadersStudy/Assets/Scripts/Boids/BoidsSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BoidsSettings : ScriptableObject
+{
+    [Header("Boid Settings")]
+    public float minSpeed = 2;
+    public float maxSpeed = 5;
+    public float sightRadius = 2.5f;
+    public float avoidRadius = 1.5f;
+    public float maxSteerForce = 3;
+
+    [Header("Boid Weight")]
+    public float alignWeight = 1;
+    public float cohesionWeight = 1;
+    public float seperateWeight = 1;
+
+    public float targetWeight = 1;
+
+    [Header("Collision")]
+    public LayerMask obstacleLayer;
+    public float boundsRadius = .35f;
+    public float avoidCollisionWeight = 10;
+    public float collisionAvoidDst = 5;
+}
